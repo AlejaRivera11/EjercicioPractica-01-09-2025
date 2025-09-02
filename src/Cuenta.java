0d1023b3a1d204b6d5b96e01a98b5da354199049
Sebastianmedellin <smedellin@estudiante.uniajc.edu.co>
2025-09-01 20:08:56 -0500

    actualizacion

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -13,12 +13,19 @@ public class Cuenta {
     }
 
     public void consignar(float cantidad){
+        this.saldo = saldo + cantidad;
     }
 
     public void retirar (float cantidad){
+        if (saldo>=cantidad){
+            this.saldo = saldo - cantidad;
+        }else{
+            System.out.println("Saldo insuficiente para este retiro");
+        }
     }
 
     public void calcularInteres(){
+        
     }
 
     public void extractoMensual(){
--- src/CuentaAhorros.java
+++ src/CuentaAhorros.java
@@ -1,3 +1,18 @@
-public class CuentaAhorros {
-    
+public class CuentaAhorros extends Cuenta {
+    protected boolean activa;
+
+    public void CuentaAhorros(float saldo, float tasa) {
+      this.saldo = saldo;
+      this.tasa = tasa;
+    }
+
+    public void retirar () {
+        if (activa == true) {
+
+        } else{
+            
+        }
+    }
+
+
 }
