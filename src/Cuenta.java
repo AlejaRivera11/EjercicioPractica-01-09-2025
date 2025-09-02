52f5de309da135838af5d15f89d63e94fde06e93
AlejaRivera11 <ariveram@estudiante.uniajc.edu.co>
2025-09-01 19:56:19 -0500

    ...

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,5 +1,28 @@
 public class Cuenta {
-    public static void main(String[] args) throws Exception {
-        
+
+    protected float saldo;
+    protected int numeroConsignaciones = 0;
+    protected int numeroRetiros = 0;
+    protected float tasaAnual;
+    protected float comisionMensual = 0;
+
+
+    public Cuenta (float saldo, float tasaAnual){
+    this.saldo = saldo;
+    this.tasaAnual = tasaAnual;
     }
+
+    public void consignar(float cantidad){
+    }
+
+    public void retirar (float cantidad){
+    }
+
+    public void calcularInteres(){
+    }
+
+    public void extractoMensual(){
+    }
+
+    
 }
